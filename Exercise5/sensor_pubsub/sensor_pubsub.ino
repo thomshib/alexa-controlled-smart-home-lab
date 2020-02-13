634e79952be69a061affa66c6188b55b5a2c44fa
Vivek Soni <erviveksoni@gmail.com>
2020-02-13 19:19:50 +0530

    Added the pub/sub code

--- Exercise5/sensor_pubsub/sensor_pubsub.ino
+++ Exercise5/sensor_pubsub/sensor_pubsub.ino
@@ -2,6 +2,7 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiUdp.h>
+#include <NTPClient.h>
 
 // Update these with values suitable for your network.
 int red_pin = 5;
@@ -11,44 +12,56 @@ int redStatus = digitalRead(red_pin);
 int greenStatus = digitalRead(green_pin);
 int blueStatus = digitalRead(blue_pin);
 
-const char* ssid = "Naina";
-const char* password = "12345678a";
+long lastMsg = 0;
+char msg[50];
+char shadowTopicUpdate[50];
+char payload[100];
+int value = 0;
+
+/**********************/
+const char *ssid = "<SSID>";
+const char *password = "<PASSWORD>";
+
+const char *thing_name = "<THING_NAME>";
+const char *AWS_endpoint = "<REST API Endpoint>";
+/**********************/
 
 WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
-const char* thing_name = "esp_client";
-const char* AWS_endpoint = "a3gqm9sszde7zz-ats.iot.ap-south-1.amazonaws.com"; //MQTT broker ip
-
-void callback(char* topic, byte* payload, unsigned int length) {
+WiFiClientSecure espClient;
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)payload[i]);
   }
   Serial.println();
 
-  if (strcmp(topic, "mysmarthome/turnonred") == 0) {
+  if (strcmp(topic, "mysmarthome/turnonred") == 0)
+  {
     set_red_led();
   }
-  if (strcmp(topic, "mysmarthome/turnonblue") == 0) {
+  if (strcmp(topic, "mysmarthome/turnonblue") == 0)
+  {
     set_blue_led();
   }
-  if (strcmp(topic, "mysmarthome/turnongreen") == 0) {
+  if (strcmp(topic, "mysmarthome/turnongreen") == 0)
+  {
     set_green_led();
   }
-  if (strcmp(topic, "mysmarthome/turnoff") == 0) {
+  if (strcmp(topic, "mysmarthome/turnoff") == 0)
+  {
     turn_off_led();
   }
 }
-
-WiFiClientSecure espClient;
 PubSubClient client(AWS_endpoint, 8883, callback, espClient); //set  MQTT port number to 8883 as per //standard
-long lastMsg = 0;
-char msg[50];
-int value = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(red_pin, OUTPUT);
   pinMode(green_pin, OUTPUT);
@@ -56,14 +69,52 @@ void setup() {
   Serial.setDebugOutput(true);
   setup_wifi();
   delay(1000);
-  if (!SPIFFS.begin()) {
+  turn_off_led();
+  snprintf(shadowTopicUpdate, 50, "$aws/things/%s/shadow/update", thing_name);
+  loadCertificates();
+}
+
+void setup_wifi()
+{
+  // We start by connecting to a WiFi network
+  espClient.setBufferSizes(512, 512);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  timeClient.begin();
+  while (!timeClient.update())
+  {
+    timeClient.forceUpdate();
+  }
+
+  espClient.setX509Time(timeClient.getEpochTime());
+}
+
+void loadCertificates()
+{
+  if (!SPIFFS.begin())
+  {
     Serial.println("Failed to mount file system");
     return;
   }
 
   // Load certificate file
   File cert = SPIFFS.open("/cert.der", "r"); //replace cert.crt with your uploaded file name
-  if (!cert) {
+  if (!cert)
+  {
     Serial.println("Failed to open cert file");
   }
   else
@@ -78,7 +129,8 @@ void setup() {
 
   // Load private key file
   File private_key = SPIFFS.open("/private.der", "r"); //replace private eith your uploaded file name
-  if (!private_key) {
+  if (!private_key)
+  {
     Serial.println("Failed to open private cert file");
   }
   else
@@ -93,7 +145,8 @@ void setup() {
 
   // Load CA file
   File ca = SPIFFS.open("/ca.der", "r"); //replace ca eith your uploaded file name
-  if (!ca) {
+  if (!ca)
+  {
     Serial.println("Failed to open ca ");
   }
   else
@@ -107,71 +160,53 @@ void setup() {
     Serial.println("ca failed");
 }
 
-void setup_wifi() {
-  // We start by connecting to a WiFi network
-  espClient.setBufferSizes(512, 512);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-   timeClient.begin();
-  while (!timeClient.update()) {
-    timeClient.forceUpdate();
-  }
-
-  espClient.setX509Time(timeClient.getEpochTime());
-}
-
-void loop() {
-
-  if (!client.connected()) {
+void loop()
+{
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
 
   long now = millis();
 
-  //  if (now - lastMsg > 2000) {
-  //    lastMsg = now;
-  //    ++value;
-  //    snprintf (msg, 75, "hello world #%ld", value);
-  ////    Serial.print("Publish message: ");
-  ////    Serial.println(msg);
-  //    //    client.publish("outTopic", msg);
-  //    Serial.print("Heap: "); Serial.println(ESP.getFreeHeap()); //Low heap can cause problems
-  //  }
+  // Publish every one second
+  if (now - lastMsg > 1000)
+  {
+    value = read_temperature();
+    lastMsg = now;
+    snprintf(msg, 75, "Room Temprature: %ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    snprintf(payload, 75, "{ \"state\":{\"reported\":{\"temperature\":%ld}}}", value);
+    //Serial.println(shadowTopicUpdate);
+    //Serial.println(payload);
+    client.publish(shadowTopicUpdate, payload);
+  }
 }
 
-
-
-
-void reconnect() {
+// Sensors and LED
+void reconnect()
+{
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect(thing_name)) {
+    if (client.connect(thing_name))
+    {
       Serial.println("connected");
       // Once connected, publish an announcement...
       //      client.publish("outTopic", "hello world");
       // ... and resubscribe
+
       client.subscribe("mysmarthome/turnoff");
       client.subscribe("mysmarthome/turnonred");
       client.subscribe("mysmarthome/turnonblue");
       client.subscribe("mysmarthome/turnongreen");
-
-    } else {
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -185,26 +220,36 @@ void reconnect() {
   }
 }
 
-void set_blue_led() {
+void set_blue_led()
+{
   digitalWrite(red_pin, HIGH);
   digitalWrite(green_pin, HIGH);
   digitalWrite(blue_pin, LOW);
 }
 
-void set_green_led() {
+void set_green_led()
+{
   digitalWrite(red_pin, HIGH);
   digitalWrite(green_pin, LOW);
   digitalWrite(blue_pin, HIGH);
 }
 
-void set_red_led() {
+void set_red_led()
+{
   digitalWrite(red_pin, LOW);
   digitalWrite(green_pin, HIGH);
   digitalWrite(blue_pin, HIGH);
 }
 
-void turn_off_led() {
+void turn_off_led()
+{
   digitalWrite(red_pin, HIGH);
   digitalWrite(green_pin, HIGH);
   digitalWrite(blue_pin, HIGH);
+}
+
+int read_temperature()
+{
+  int temp = 23;
+  return temp;
 }
