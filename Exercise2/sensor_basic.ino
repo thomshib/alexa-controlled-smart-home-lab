6187517d48cfb220e2fe8f7ac6c83d53c8b123b5
Daniel Euchar <daniel.euchar@thoughtworks.com>
2020-02-18 08:00:32 +0530

    added temperature sensor | Fixed pin declaration in web server

--- Exercise2/sensor_basic.ino
+++ Exercise2/sensor_basic.ino
@@ -1,56 +1,30 @@
-#include <ESP8266WiFi.h>
+#include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
+ 
 
 // GPIO Pins
-int red = 5;
-int green = 4;
-int blue = 0;
-int redStatus = digitalRead(red);
-int greenStatus = digitalRead(green);
-int blueStatus = digitalRead(blue);
+int red_pin = 5;
+int green_pin = 4;
+int blue_pin = 0;
 
-// Update these with values suitable for your network configuratons.
-/**********************/
-const char* ssid = "<SSID>";
-const char* password = "<PASSWORD>";
-/**********************/
+//Set Temperature sensor to read from GPIO 2 / D4 
+#define DHTTYPE DHT11
+#define dht_dpin 2
+DHT dht(dht_dpin, DHTTYPE);
 
-WiFiServer server(80);
+int redStatus = digitalRead(red_pin);
+int greenStatus = digitalRead(green_pin);
+int blueStatus = digitalRead(blue_pin);
+float temperature = 0;
+float humidity = 0;
 
-void setup_wifi()
-{
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("Connected to Wifi");
-}
-
-void configure_local_web_server()
-{
-    //Start WiFi server
-  server.begin();
-  Serial.println("Server started!");
-
-  // Print the IP address
-  Serial.print("Use this URL to connect: http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
-}
 
 void setup() 
 {
+  dht.begin();
   Serial.begin(115200);
-  pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);
-  setup_wifi();
-  configure_local_web_server();
+  pinMode(red_pin, OUTPUT);
+  pinMode(green_pin, OUTPUT);
+  pinMode(blue_pin, OUTPUT);
 }
 
 void set_blue_led()
@@ -80,63 +54,30 @@ void turn_off_led()
   digitalWrite(green_pin, HIGH);
   digitalWrite(blue_pin, HIGH);
 }
+
+void get_led_status(){
+  redStatus = digitalRead(red_pin);
+  greenStatus = digitalRead(green_pin);
+  blueStatus = digitalRead(blue_pin);
+}
+
+void get_temperature(){
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+}
+
 void loop() {
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
+  set_blue_led();
+  delay(1000);
+  set_green_led();
+  delay(1000);
+  set_red_led();
+  delay(1000);
+  turn_off_led();
+  get_temperature();
+  Serial.print("Humidity :" );
+  Serial.print(humidity);
+  Serial.print(" | Temperature :" );
+  Serial.println(temperature);
 
-  // Wait until the client sends some data
-  while (!client.available()) {
-    delay(1);
-  }
-
-  // Read the first line of the request
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
-
-  // Match the request
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println("Connection: close");
-  client.println(""); //  do not forget this one
-  client.println("<!DOCTYPE html><html>");
-
-  if (request.indexOf("/RED=ON") != -1)  {
-    Serial.println("RED is HIGH" );
-    set_red_led();
-    redStatus = digitalRead(red);
-  }
-  if (request.indexOf("/GREEN=ON") != -1)  {
-    Serial.println("GREEN is HIGH" );
-    set_green_led();
-    greenStatus = digitalRead(green);
-  }
-  if (request.indexOf("/BLUE=ON") != -1)  {
-    Serial.println("BLUE is HIGH" );
-    set_blue_led();
-    blueStatus = digitalRead(blue);
-  }
-  if (request.indexOf("/TURNOFF=OFF") != -1)  {
-    Serial.println("LED is LOW" );
-    turn_off_led();
-    redStatus = digitalRead(red);
-    greenStatus = digitalRead(green);
-    blueStatus = digitalRead(blue);
-  }
-
-  client.print("~~~ Led Status ~~~ ");
-  client.print("RED: "); client.println(redStatus);
-  client.print("GREEN: "); client.println(greenStatus);
-  client.print("BLUE: "); client.println(blueStatus);
-
-  client.println("<br><br>");
-  client.println("<a href=\"/RED=ON\"\"><button>RED ON</button></a>");
-  client.println("<a href=\"/GREEN=ON\"\"><button>GREEN ON</button></a>");
-  client.println("<a href=\"/BLUE=ON\"\"><button>BLUE ON</button></a>");
-  client.println("<a href=\"/TURNOFF=OFF\"\"><button>TURN OFF</button></a>");
-  client.println("</html>");
-  delay(1);
 }
--- Exercise3/sensor_web_server.ino
+++ Exercise3/sensor_web_server.ino
@@ -1,20 +1,34 @@
 #include <ESP8266WiFi.h>
+#include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
 
-int red = 5;
-int green = 4;
-int blue = 0;
-int redStatus = digitalRead(red);
-int greenStatus = digitalRead(green);
-int blueStatus = digitalRead(blue);
+// GPIO Pins
+int red_pin = 5;
+int green_pin = 4;
+int blue_pin = 0;
+
+//Set Temperature sensor to read from pin 2 / D4 
+#define DHTTYPE DHT11
+#define dht_dpin 2
+DHT dht(dht_dpin, DHTTYPE);
+
+//declare global variables 
+int redStatus = digitalRead(red_pin);
+int greenStatus = digitalRead(green_pin);
+int blueStatus = digitalRead(blue_pin);
+float temperature = 0;
+float humidity = 0;
+
+// Update these with values suitable for your network configuratons.
+/**********************/
 const char* ssid = "<SSID>";
 const char* password = "<PASSWORD>";
+/**********************/
+// Set web server port number to 80
 WiFiServer server(80);
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);
+void setup_wifi()
+{
+  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -25,8 +39,11 @@ void setup() {
   }
   Serial.println("");
   Serial.println("Connected to Wifi");
+}
 
-  //Start WiFi server
+void configure_local_web_server()
+{
+    //Start WiFi server
   server.begin();
   Serial.println("Server started!");
 
@@ -36,26 +53,57 @@ void setup() {
   Serial.println("/");
 }
 
-void Red() {
-  digitalWrite(red, LOW);
-  digitalWrite(green, HIGH);
-  digitalWrite(blue, HIGH);
+void setup() 
+{
+  dht.begin();
+  Serial.begin(115200);
+  pinMode(red_pin, OUTPUT);
+  pinMode(green_pin, OUTPUT);
+  pinMode(blue_pin, OUTPUT);
+  setup_wifi();
+  configure_local_web_server();
 }
-void Green() {
-  digitalWrite(red, HIGH);
-  digitalWrite(green, LOW);
-  digitalWrite(blue, HIGH);
+
+void set_blue_led()
+{
+  digitalWrite(red_pin, HIGH);
+  digitalWrite(green_pin, HIGH);
+  digitalWrite(blue_pin, LOW);
 }
-void Blue() {
-  digitalWrite(red, HIGH);
-  digitalWrite(green, HIGH);
-  digitalWrite(blue, LOW);
+
+void set_green_led()
+{
+  digitalWrite(red_pin, HIGH);
+  digitalWrite(green_pin, LOW);
+  digitalWrite(blue_pin, HIGH);
 }
-void Off() {
-  digitalWrite(red, HIGH);
-  digitalWrite(green, HIGH);
-  digitalWrite(blue, HIGH);
+
+void set_red_led()
+{
+  digitalWrite(red_pin, LOW);
+  digitalWrite(green_pin, HIGH);
+  digitalWrite(blue_pin, HIGH);
 }
+
+void turn_off_led()
+{
+  digitalWrite(red_pin, HIGH);
+  digitalWrite(green_pin, HIGH);
+  digitalWrite(blue_pin, HIGH);
+}
+
+
+void get_led_status(){
+  redStatus = digitalRead(red_pin);
+  greenStatus = digitalRead(green_pin);
+  blueStatus = digitalRead(blue_pin);
+}
+
+void get_temperature(){
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+}
+
 void loop() {
   // Check if a client has connected
   WiFiClient client = server.available();
@@ -82,31 +130,30 @@ void loop() {
 
   if (request.indexOf("/RED=ON") != -1)  {
     Serial.println("RED is HIGH" );
-    Red();
-    redStatus = digitalRead(red);
-    greenStatus = 0;
-    blueStatus = 0;
+    set_red_led();
   }
   if (request.indexOf("/GREEN=ON") != -1)  {
     Serial.println("GREEN is HIGH" );
-    Green();
-    greenStatus = digitalRead(green);
-    redStatus = 0;
-    blueStatus = 0;
+    set_green_led();
+    
   }
   if (request.indexOf("/BLUE=ON") != -1)  {
     Serial.println("BLUE is HIGH" );
-    Blue();
-    blueStatus = digitalRead(blue);
-    redStatus = 0;
-    greenStatus = 0;
+    set_blue_led();   
   }
-  if (request.indexOf("/OFF=OFF") != -1)  {
-    Off();
+  if (request.indexOf("/TURNOFF=OFF") != -1)  {
+    Serial.println("LED is LOW" );
+    turn_off_led();
   }
+  
+  get_temperature();
+  client.print("<strong>~~~ Temperature Sensor ~~~ </strong><br>");
+  client.print("Temperature: "); client.println(temperature);
+  client.print("<br>Humidity: "); client.println(humidity);
 
-  client.print("Led Status : ");client.println();
-  client.print("RED: "); client.println(redStatus);
+  get_led_status();
+  client.print("<br><br><strong>~~~ Led Status ~~~ </strong><br>");
+  client.println("RED: "); client.println(redStatus);
   client.print("GREEN: "); client.println(greenStatus);
   client.print("BLUE: "); client.println(blueStatus);
 
@@ -114,11 +161,7 @@ void loop() {
   client.println("<a href=\"/RED=ON\"\"><button>RED ON</button></a>");
   client.println("<a href=\"/GREEN=ON\"\"><button>GREEN ON</button></a>");
   client.println("<a href=\"/BLUE=ON\"\"><button>BLUE ON</button></a>");
-  client.println("<a href=\"/OFF=OFF\"\"><button>OFF</button></a>");
+  client.println("<a href=\"/TURNOFF=OFF\"\"><button>TURN OFF</button></a>");
   client.println("</html>");
-
-
-
   delay(1);
-
 }
